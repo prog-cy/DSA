736aa5a8a63387f03b6627e5589e583a69795394
rupeshdwivedi3945@gmail.com <rupeshdwivedi3945@gmail.com>
2021-08-10 11:48:12 +0530

    change have been made in Recursion folder

---
+++ Recursion/src/recursion/FibbonacciNumber.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class FibbonacciNumber {
+	
+	//method to find the fibonacci series
+	static int fib(int n) {
+		
+		if(n == 0 || n == 1)return 1;
+		
+		return fib(n-1) + fib(n-2);
+	}
+	
+	//main method
+	public static void main(String[] args) {
+		
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		
+		System.out.println("Enter how many terms of fibonacci series you want: ");
+		int term = sc.nextInt();
+		sc.close();
+		
+		System.out.println("Fibonacci series having "+term+" terms");
+		for(int i = 0; i<term; i++){
+			System.out.print(fib(i)+" ");
+		}
+	}
+
+}

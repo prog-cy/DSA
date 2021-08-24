a84c5ec81c6de58714f97b180addf2da061c7e62
rupeshdwivedi3945@gmail.com <rupeshdwivedi3945@gmail.com>
2021-08-24 06:49:20 +0530

    Tree directory modified

--- Tree/src/binarysearchtree/BST1.java
+++ Tree/src/binarysearchtree/BST1.java
@@ -33,7 +33,7 @@ public class BST1 {
 		
 		if(root == null) return false;
 		else if(root.data == key) return true;
-		else if(key <= root.data) return search(root.left, key);
+		else if(key < root.data) return search(root.left, key);
 		else return search(root.right, key);
 		
 	}
@@ -50,6 +50,73 @@ public class BST1 {
 		inorder(root.right);
 	}
 	
+	//method to get the minimum value in the BST
+	static int getMin(Node root) {
+		
+		if(root.left == null) return root.data;
+		return getMin(root.left);
+	}
+	
+	//method to get the maximum value in the BST
+	static int getMax(Node root) {
+		
+		if(root.right == null) return root.data;
+		return getMax(root.right);
+	}
+	
+	//method to get sum of all the nodes
+	static int getTotalSum(Node root) {
+		
+		if(root == null) return 0;
+		return root.data + getTotalSum(root.left) + getTotalSum(root.right);
+	}
+	
+	//method to count total number of nodes in the tree
+	static int countTotalNode(Node root) {
+		
+		if(root == null) return 0;
+		return 1 + countTotalNode(root.left) + countTotalNode(root.right);
+	}
+	
+	//method to get the height of the tree
+	static int getHeight(Node root) {
+		
+		if(root == null) return 0;
+		return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+	}
+	//method to count total number of leaf nodes in the tree
+	static int countLeaf(Node root) {
+		if(root == null) return 0;
+		else if(root.left == null && root.right == null)return 1;
+		
+		return countLeaf(root.left) + countLeaf(root.right );
+	}
+	
+	//this method is used to print nodes at any level
+	static void printNodesAtLevel(Node root, int level) {
+		
+		if(root == null) return;
+		
+		else if(level == 0) {
+			System.out.print(root.data+" ");
+			return;
+		}
+		printNodesAtLevel(root.left, level - 1);
+		printNodesAtLevel(root.right, level - 1);
+	}
+	
+	//method to traverse a tree in level order
+	static void levelorder(Node root) {
+		
+		int h = getHeight(root);
+		
+		for(int i = 0; i<h; i++) {
+
+			printNodesAtLevel(root, i);
+			System.out.println();
+		}
+	}
+	//main method
 	public static void main(String[] args) {
 		
 		Node root = insert(null, 15);
@@ -60,16 +127,26 @@ public class BST1 {
 		insert(root, 90);
 		insert(root, 12);
 		insert(root, 78);
+		insert(root, 120);
 		
-		
+		System.out.print("Inorder: ");
 		inorder(root);
 		
-		System.out.println();
+		System.out.println("\nLevel order: ");
+		levelorder(root);
 		
+		System.out.println();
 		if(search(root, 90))
-			System.out.println("present");
+			System.out.println(90+" is present");
 		else
 			System.out.println("Not present");
+		
+		System.out.println("Minimum value in tree: "+getMin(root));
+		System.out.println("Maximum value in tree: "+getMax(root));
+		System.out.println("Sum of all the nodes in the tree: "+getTotalSum(root));
+		System.out.println("Total number of nodes in the tree: "+countTotalNode(root));
+		System.out.println("Total number of leaf nodes in the tree: "+countLeaf(root));
+		System.out.println("Height of the tree: "+getHeight(root));
 	}
 
 }
---
+++ Tree/src/binarytree/GenericBinaryTree.java
@@ -0,0 +1,219 @@
+package binarytree;
+
+
+class Node<T>{
+	
+	T data;
+	
+	Node<T> left, right;
+	
+	Node(T data){this.data = data;}
+}
+
+class BinaryTree<T> extends Node<T>{
+	
+	//this is constructor which calls the Node<T> class contructor 
+	BinaryTree(T data){
+		super(data);
+	}
+	
+	BinaryTree() {
+		this(null);
+	}
+	
+	 static java.util.Scanner sc = new java.util.Scanner(System.in);
+	 
+	 boolean isInteger(String s) {
+		 
+		 boolean check = false;
+		 
+		 for(int i = 0; i<s.length(); i++) {
+			 
+			 Character ch = s.charAt(i);
+			 
+			 if(Character.isDigit(ch))
+				 check = true;
+		 }
+		 
+		 return check;
+	 }
+	 
+	 
+	 Integer getInput(String str) {
+		 
+		 if(isInteger(str))
+			 return Integer.parseInt(str); 
+		 else
+			 return 0;
+	 }
+	 
+	
+	 //method to create a binary tree
+	 @SuppressWarnings("unchecked")
+	 Node<T> createBT() {
+		
+		System.out.println("Enter data: ");
+		
+		String str = sc.next();
+		
+		T data = (T) getInput(str);
+		
+		if((Integer)data == -1)return null;
+		
+		Node<T> node = new BinaryTree<>(data);
+		
+		System.out.println("Enter left node of "+node.data);
+		node.left = createBT();
+		
+		System.out.println("Enter right node of "+node.data);
+		node.right = createBT();
+		
+		return node;
+	}
+	 
+	//Declaring a stack
+	 java.util.Stack<Node<T>> stack = new java.util.Stack<Node<T>>();
+	 
+	//method to traverse the tree in preorder
+	 void preorder(Node<T> root) {
+		 
+		stack.push(root);
+		
+		while(!stack.empty()) {
+			
+			Node<T> current = stack.pop();
+			
+			System.out.print(current.data+" ");
+			
+			if(current.right != null)stack.push(current.right);
+			if(current.left != null)stack.push(current.left);
+			
+		}
+	 }
+	 
+	 //method to traverse the tree in Inorder
+	 void inorder(Node<T> root) {
+		 
+		 Node<T> temp = root;
+		 
+		 while(true) {
+			 
+			 while(temp.left != null) {
+				 stack.push(temp);
+				 temp = temp.left;
+			 }
+			 
+			 while(temp.right == null) {
+				 
+				 System.out.print(temp.data+" ");
+				 if(stack.empty())return;
+				 temp = stack.pop();
+			 }
+			 
+			 System.out.print(temp.data+" ");
+			 temp = temp.right;
+		 }
+	 }
+	 
+	 //method to traverse the tree in postorder
+	 void postorder(Node<T> root) {
+		 
+		 Node<T> q = root;
+		 Node<T> temp = root;
+		 
+		 while(true) {
+			 
+			 while(temp.left != null) {
+				 stack.push(temp);
+				 temp = temp.left;
+			 }
+			 
+			 while(temp.right == null || temp.right == q) {
+				 
+				 System.out.print(temp.data+" ");
+				 q = temp;
+				 
+				 if(stack.empty()) return;
+				 temp = stack.pop();
+			 }
+			 
+			 System.out.print(temp.data+" ");
+			 temp = temp.right;
+		 }
+	 }
+	 
+	 //method to traverse the tree in Level order
+	 void levelorder(Node<T> root) {
+		 
+		 java.util.Queue<Node<T>> q = new java.util.LinkedList<>();
+		 q.add(root);
+		 
+		 while(!q.isEmpty()) {
+			 
+			 Node<T> current = q.peek();
+			 
+			 System.out.print(current.data+" ");
+			 if(current.left != null)q.add(current.left);
+			 if(current.right != null)q.add(current.right);
+			 q.remove();
+		 }
+		 	 
+	 }
+	 
+	 //method to find the height of the tree
+	 int getHeight(Node<T> root) {
+		 
+		 if(root == null) return 0;
+		 return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+	 }
+	 
+	 //method to find the leaf node in the tree
+	 int getLeafNodes(Node<T> root) {
+		 
+		 if(root == null) return 0;
+		 if(root.left == null && root.right == null) return 1;
+		 return getLeafNodes(root.left) + getLeafNodes(root.right);
+	 }
+	 
+	 //method to find the total node in the tree
+	 int getTotalNodes(Node<T> root) {
+		 
+		 if(root == null) return 0;
+		 return 1 + getTotalNodes(root.left) + getTotalNodes(root.right);
+	 }
+	 
+	 //method to find the internal node in the tree
+	 int getInternalNodes(Node<T> root) {
+		 
+		 return getTotalNodes(root) - getLeafNodes(root);
+	 }
+	 
+}
+
+//Main class
+public class GenericBinaryTree{
+	
+	public static void main(String[] args) {
+		
+		BinaryTree<Integer> btree = new BinaryTree<>();
+		
+		System.out.println("Create a binary tree: ");
+		Node<Integer> root = btree.createBT();
+		
+		System.out.print("\nPreorder: ");
+		btree.preorder(root);
+		
+		System.out.print("\nPostorder: ");
+		btree.postorder(root);
+		
+		System.out.print("\nInorder: ");
+		btree.inorder(root);
+		
+		System.out.print("\nLevel order: ");
+		btree.levelorder(root);
+		
+		System.out.println("\nHeight of the tree: "+btree.getHeight(root));
+		System.out.println("Total number of leaf nodes: "+btree.getLeafNodes(root));
+	}
+
+}

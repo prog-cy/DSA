f9305c9ab1878280182e9687bcae9bd5213e60f2
rupeshdwivedi3945@gmail.com <rupeshdwivedi3945@gmail.com>
2021-09-21 14:07:56 +0530

    modified

--- Tree/src/heap/MaxHeap4.java
+++ Tree/src/heap/MaxHeap4.java
@@ -14,14 +14,46 @@ public class MaxHeap4 {
 		
 		heap.add(key);
 		
+		int i = heap.size() - 1;
 		
+		while(i > 1) {
+			
+			int parent = i/2;
+			
+			if(heap.get(parent) < heap.get(i)) {
+				
+				int temp = heap.get(parent);
+				
+				heap.set(parent, heap.get(i));
+				heap.set(i, temp);
+			}
+			
+			i = parent;
+		}
+	}
+	
+	//method to display the heap in level order
+	static void levelorder() {
 		
+		for(int i = 1; i<heap.size(); i++)
+			System.out.print(heap.get(i)+" ");
 	}
 	
 	// main method
 	public static void main(String[] args) {
 		
 		heap.add(Integer.MIN_VALUE);
+		
+		insert(10);
+		insert(40);
+		insert(50);
+		insert(809);
+		insert(20);
+		insert(-10);
+		
+		
+		System.out.print("Level order traversal of the heap: ");
+		levelorder();
 	}
 
 }
